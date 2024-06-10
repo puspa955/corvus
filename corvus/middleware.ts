982ee3b0bf62f536fc60298e066fa0437e9d2821
puspa <limbup503@gmail.com>
2024-06-10 20:54:03 +0545

    middle

--- corvus/middleware.ts
+++ corvus/middleware.ts
@@ -1,13 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Define your middleware
-const middleware = clerkMiddleware();
 
-// Export the middleware
-export default middleware;
+export default clerkMiddleware({
+  
+});
 
-// Define additional configuration if needed
 export const config = {
-  // Define your matcher patterns
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
