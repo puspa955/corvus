b1497c857705f36b88c3d27e0b12e9d2e0a3a6de
puspa <limbup503@gmail.com>
2024-06-10 19:52:57 +0545

    undefined

--- corvus/app/(setup)/page.tsx
+++ corvus/app/(setup)/page.tsx
@@ -3,23 +3,30 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { InitialModal } from "@/components/modals/initial-modal";
 
-const SetupPage = async() => {
-const profile = await initialProfile();
+const SetupPage = async () => {
+  const profile = await initialProfile();
 
-const server = await db.server.findFirst({
-    where:{
-        members:{
-            some:{
-                profileId: profile.id
-            }
+  if (!profile) {
+    // Handle the case where profile is undefined
+    console.error("Profile is undefined");
+    return <div>No profile available</div>;
+  }
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile?.id
         }
+      }
     }
-});
-if (server) {
-    return redirect (`/servers/${server.id}`);
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
 }
 
-    return <InitialModal/>;
-}
- 
 export default SetupPage;
