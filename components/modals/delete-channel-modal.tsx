8ba493039c610bcc3ccafb0b05a9bb31d092c411
puspa <limbup503@gmail.com>
2024-06-24 10:02:30 +0545

    change

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -15,7 +15,7 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export const DeleteChannelModal = () => {
     const{ isOpen, onClose, type, data} = useModal();
